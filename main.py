7d935b85ef7fca9571cd16e0b61b749c12e77d75
Wongsakorn Pinvasee <wongsakorn.pi@ku.th>
2021-02-18 16:18:25 +0700

    LOAD

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import configparser
-import sqlalchemy
 import pandas as pd
 from sqlalchemy.orm import sessionmaker
 import requests
 import json
 from datetime import datetime
 import datetime
-import sqlite3
+import pymysql
 
 # read config file
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+#set config
+class Config:
+    MYSQL_HOST = config['mysql']['host']
+    MYSQL_PORT = config['mysql'].getint('port')
+    MYSQL_USER = config['mysql']['user']
+    MYSQL_PASSWORD = ""
+    MYSQL_DB = config['mysql']['db']
+    MYSQL_CHARSET = 'utf8mb4'
 
 DATABASE_LOCATION = 'sqlite:///my_played_tracks.sqlite'
 USER_ID = config['spotify']['user_id']
@@ -33,9 +41,39 @@ def check_valid_data(df: pd.DataFrame):
 
     return True
 
+# Load dataframe to mysql
+def dataframe_to_mysql(df: pd.DataFrame):
+    try:
+        #connect to sql
+        connection = pymysql.connect(host=Config.MYSQL_HOST,
+                                    port=Config.MYSQL_PORT,
+                                    user=Config.MYSQL_USER,
+                                    password=Config.MYSQL_PASSWORD,
+                                    db=Config.MYSQL_DB,
+                                    charset=Config.MYSQL_CHARSET)
+
+        cursor = connection.cursor()
+
+        # insert data
+        for(row,s) in df.iterrows():
+            song = s[0]
+            artist = s[1]
+            played_at = s[2]
+            timestamp = s[3]
+            
+            query = f'INSERT INTO my_song VALUES("{song}","{artist}","{played_at}","{timestamp}")'
+            cursor.execute(query)
+
+        connection.commit()
+        cursor.close()
+    except:
+        print('Error in Mysql connect')
+    finally:
+        connection.close()
+
 if __name__ == '__main__':
 
-    # Extract
+    # EXTRACT
     headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
@@ -74,11 +112,15 @@ if __name__ == '__main__':
                             columns = ['song', 'artist', 'played_at', 'timestamp'])
 
 
-    # transform
+    # TRANSFORM
     if check_valid_data(song_df):
         print("Data valid")
         
-        # get only song that played yesterday
-        excep = pd.to_datetime(song_df['timestamp'], format='%Y-%m-%d') == str(yesterday)[0:10]
-        song_df = song_df[excep]
-        print(song_df)
+     # get only song that played yesterday
+    excep = pd.to_datetime(song_df['timestamp'], format='%Y-%m-%d') == str(yesterday)[0:10]
+    song_df = song_df[excep]
+
+
+    # LOAD
+    dataframe_to_mysql(song_df)
+    print("Data transferred to mysql successfully")
