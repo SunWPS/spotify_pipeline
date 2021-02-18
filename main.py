2572b369f0cbcf36c3e2b6a5d540d8f9ba3420c2
Wongsakorn Pinvasee <wongsakorn.pi@ku.th>
2021-02-18 11:39:45 +0700

    Transform

--- main.py
+++ main.py
@@ -16,13 +16,30 @@ DATABASE_LOCATION = 'sqlite:///my_played_tracks.sqlite'
 USER_ID = config['spotify']['user_id']
 TOKEN = config['spotify']['token']
 
+def check_valid_data(df: pd.DataFrame):
+    # check if df is empty
+    if df.empty:
+        print("No songs dowloaded.")
+        return False
+
+    # check if primary key isn't unique
+    if pd.Series(df['played_at']).is_unique:
+        pass
+    else:
+        raise Exception("PK check is violated")  
+
+    if df.isnull().values.any():
+        raise Exception("Null valued found")
+
+    return True
+
 if __name__ == '__main__':
 
     # Extract
     headers = {
-        'Accept' : 'application/json',
-        'Content-Type' : 'application/json',
-        'Authorization' : 'Bearer {token}'.format(token=TOKEN)
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {token}'.format(token=TOKEN)
     }
 
     today = datetime.datetime.now()
@@ -30,6 +47,7 @@ if __name__ == '__main__':
     yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
     url = 'https://api.spotify.com/v1/me/player/recently-played?after={time}'.format(time=yesterday_unix_timestamp)
+
     r = requests.get(url, headers=headers)
 
     data = r.json()
@@ -39,7 +57,6 @@ if __name__ == '__main__':
     played_at = []
     timestamps = []
 
-
     for song in data['items']:
         songs.append(song['track']['name'])
         artists.append(song['track']['album']['artists'][0]['name'])
@@ -47,11 +64,21 @@ if __name__ == '__main__':
         timestamps.append(song['played_at'][0:10])
            
     song_dict = {
-        'song' : songs,
+        'song': songs,
         'artist': artists,
-        'played_at' : played_at,
-        'timestamp' : timestamps
+        'played_at': played_at,
+        'timestamp': timestamps
     }
 
-    song_df = pd.DataFrame(song_dict, columns = ['song', 'artist', 'played_at', 'timestamp'])
-    print(song_df.head())
+    song_df = pd.DataFrame(song_dict, 
+                            columns = ['song', 'artist', 'played_at', 'timestamp'])
+
+
+    # transform
+    if check_valid_data(song_df):
+        print("Data valid")
+        
+        # get only song that played yesterday
+        excep = pd.to_datetime(song_df['timestamp'], format='%Y-%m-%d') == str(yesterday)[0:10]
+        song_df = song_df[excep]
+        print(song_df)
